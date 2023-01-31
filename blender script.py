c56aa9bc8939a25fd9ed8b8c365c0f6f006341a0
Guyot Florian <florian.guyot@isi.fraunhofer.de>
2023-01-31 16:14:37 +0100

    Worked on duplicating the materials

--- blender script.py
+++ blender script.py
@@ -1,9 +1,12 @@
 import bpy
 
+file_path = "C:\\Users\\axelf\\PycharmProjects\\Blender-python-code\\Build a Castle.mp3"
+
 nb_barres = 20
 meter = "Meter."
 bpy.data.scenes['Scene'].frame_set(0)
 tempLoaction = (0, 0, 1)
+zLocation = bpy.data.objects["Aspect"].location.z
 bpy.data.objects["Aspect"].location = tempLoaction
 for i in range(1, nb_barres):
 
@@ -35,35 +38,30 @@ for i in range(1, nb_barres):
     bpy.data.objects[meter + str(i + 1)].select_set(True)
 
     bpy.context.object.modifiers["Hook-Empty"].object = bpy.data.objects["Aspect"]
+    
+    
+    
+    # TO DO copy materials into objects and link the sound as f-curve
+    
+    
 
     ob = bpy.context.active_object
-    ob.data.materials[0] = bpy.data.materials.get("Meter material Full.001")
+    ob.data.materials[0] = bpy.data.materials.get("Meter material Full.001").copy()
 
-    nodes = bpy.data.materials.get("Meter material Full.001").node_tree.nodes
-    node = nodes['Value.002']
+    node_tree = bpy.data.materials.get("Meter material Full.001").node_tree.nodes
+    value_node = node_tree['Value.002']
 
     #    mat = bpy.data.materials.get("Meter material Full.001")
 
     #    mat.node_tree.nodes["Music"].inputs[0].default_value = 0.5
+    original_type = bpy.context.area.type
+    bpy.context.area.type = "NODE_EDITOR"
+    value_node.active
+    bpy.context.area.type = "GRAPH_EDITOR"
+    bpy.ops.graph.sound_bake(filepath=file_path, low=100000, high=20000, attack=0.2)
+    bpy.context.area.type = original_type
+    
 
-    nodes.node.outputs[0].value = bpy.ops.graph.sound_bake(filepath="C:\\Users\\axelf\\PycharmProjects\\Blender-python-code\\Build a Castle.mp3", low=20, high=20000, attack=0.2)
-
-
-#    bpy.data.materials["Meter material Full.001"].node_tree.nodes["Value.002"].outputs[0].value = bpy.ops.graph.sound_bake(filepath="G:\\Perso\\Musiques\\Hades - In the Blood (ft. Ashley Barrett).mp3", low=20, high=20000)
-
-
-#    for node in nodes:
-#        node.select = False
-#    node.select = True
-#    nodes.active = node
-
-
-#    bpy.ops.graph.sound_bake(filepath="G:\\Perso\\Musiques\\Hades - In the Blood (ft. Ashley Barrett).mp3", low=20, high=20000)
-
-bpy.data.objects["Aspect"].location = (0, 0, 0.25)
-
-# bpy.ops.object.select_all(action='DESELECT')
-# bpy.data.objects["Meter.1"].select_set(True)
-# obj.hide_set(state)
+bpy.data.objects["Aspect"].location.z = zLocation
 
 
