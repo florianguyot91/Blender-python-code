6035688995300c3ea5600119ff7b3ed29a10990b
Guyot Florian <florian.guyot@isi.fraunhofer.de>
2023-04-12 21:28:54 +0200

    Resolved path & crashes issues

--- audio_import.py
+++ audio_import.py
@@ -1,13 +1,16 @@
 import bpy
 
 # Set the filepath to the audio file
-audio_filepath = "G:\Perso\Documents\Blender_projects\Tests\Blender-python-code\Build_a_Castle.mp3"
+audio_filepath = ".\Blender-python-code\Build_a_Castle.mp3"
 
 # Switch to the VSE workspace
 bpy.context.area.ui_type = 'SEQUENCE_EDITOR'
 
 # Add a new audio strip to the VSE
-bpy.ops.sequencer.sound_strip_add(filepath=audio_filepath, frame_start=1, channel=1)
+bpy.ops.sequencer.sound_strip_add(filepath="//Build_a_Castle.mp3",
+                                  directory="Blender-python-code\\",
+                                  files=[{"name": "Build_a_Castle.mp3", "name": "Build_a_Castle.mp3"}],
+                                  relative_path=True, frame_start=0, channel=1)
 
 # Get the audio strip from the VSE
 audio_strip = bpy.data.scenes["Scene"].sequence_editor.sequences_all["Build_a_Castle.mp3"]
@@ -17,3 +20,5 @@ audio_strip.frame_final_end = int(audio_strip.frame_start + audio_strip.frame_fi
 
 # Set the scene end frame to match the end of the audio strip
 bpy.context.scene.frame_end = audio_strip.frame_final_end
+bpy.context.area.ui_type = 'TEXT_EDITOR'
+
--- blender script.py
+++ blender script.py
@@ -28,16 +28,16 @@ def get_context_area(context, context_dict, area_type='GRAPH_EDITOR',
 
 
 nb_barres = 20
-meter = "Meter."
+bar_spacing_x = 1.1
 bpy.data.scenes['Scene'].frame_set(0)
 tempLoaction = (0, 0, 1)
 zLocation = bpy.data.objects["Aspect"].location.z
 bpy.data.objects["Aspect"].location = tempLoaction
 for i in range(1, nb_barres):
 
-    bpy.data.objects[meter + str(i)].select_set(True)
+    # bpy.data.objects["Meter." + str(i)].select_set(True)
     bpy.ops.object.duplicate_move_linked(OBJECT_OT_duplicate={"linked": True, "mode": 'TRANSLATION'},
-                                         TRANSFORM_OT_translate={"value": (1.1, 0, 0), "orient_axis_ortho": 'X',
+                                         TRANSFORM_OT_translate={"value": (bar_spacing_x, 0, 0), "orient_axis_ortho": 'X',
                                                                  "orient_type": 'GLOBAL',
                                                                  "orient_matrix": ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
                                                                  "orient_matrix_type": 'GLOBAL',
@@ -57,10 +57,10 @@ for i in range(1, nb_barres):
     bpy.ops.object.make_single_user(object=True, obdata=True, material=True, animation=False, obdata_animation=False)
 
     for obj in bpy.context.selected_objects:
-        obj.name = meter + str(i + 1)
+        obj.name = "Meter." + str(i + 1)
 
     bpy.ops.object.select_all(action='DESELECT')
-    bpy.data.objects[meter + str(i + 1)].select_set(True)
+    bpy.data.objects["Meter." + str(i + 1)].select_set(True)
 
     bpy.context.object.modifiers["Hook-Empty"].object = bpy.data.objects["Aspect"]
     
@@ -82,7 +82,7 @@ for i in range(1, nb_barres):
 
     bpy.data.materials["Meter material Full." + str(i + 1)].node_tree.nodes["Value.002"].outputs[0].keyframe_insert(data_path='default_value', frame=1)
 
-    bpy.ops.graph.sound_bake(filepath=file_path, low=100000, high=20000, attack=0.2)
+    # bpy.ops.graph.sound_bake(filepath=file_path, low=100000, high=20000, attack=0.2)
 
 bpy.data.objects["Aspect"].location.z = zLocation
 bpy.context.area.type = original_type
