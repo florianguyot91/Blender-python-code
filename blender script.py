ce21c8af9c37cd4aefc5dd4d7445d57cc9260fd1
Guyot Florian <florian.guyot1@epfedu.fr>
2023-04-14 16:50:52 +0200

    Duplicating bars issue resolved (problem being changing context.area.type to go to a previosly loaded one created issues)

--- blender script.py
+++ blender script.py
@@ -5,14 +5,26 @@ file_path = os.path.join(os.path.dirname(bpy.data.filepath), "Build_a_Castle.mp3
 original_type = bpy.context.area.type
 nb_barres = 20
 bar_spacing_x = 1.1
+meter = "Meter."
 bpy.data.scenes['Scene'].frame_set(0)
 tempLoaction = (0, 0, 1)
 zLocation = bpy.data.objects["Aspect"].location.z
 bpy.data.objects["Aspect"].location = tempLoaction
+
+context = bpy.context
+
+
+for area in context.screen.areas:
+    if area != context.area:
+        break
+
+override = {'region': area.regions[0]}
+
 for i in range(1, nb_barres):
 
-    # bpy.data.objects["Meter." + str(i)].select_set(True)
-    bpy.ops.object.duplicate_move_linked(OBJECT_OT_duplicate={"linked": True, "mode": 'TRANSLATION'},
+    bpy.data.objects[meter + str(i)].select_set(True)
+    context.area.type = 'VIEW_3D'
+    bpy.ops.object.duplicate_move_linked(override, OBJECT_OT_duplicate={"linked": True, "mode": 'TRANSLATION'},
                                          TRANSFORM_OT_translate={"value": (1.1, 0, 0), "orient_axis_ortho": 'X',
                                                                  "orient_type": 'GLOBAL',
                                                                  "orient_matrix": ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
@@ -23,23 +35,20 @@ for i in range(1, nb_barres):
                                                                  "proportional_size": 1,
                                                                  "use_proportional_connected": False,
                                                                  "use_proportional_projected": False, "snap": False,
-                                                                 "snap_elements": {'INCREMENT'},
-                                                                 "use_snap_project": False, "snap_target": 'CLOSEST',
-                                                                 "use_snap_self": True, "use_snap_edit": True,
-                                                                 "use_snap_nonedit": True, "use_snap_selectable": False,
-                                                                 "snap_point": (0, 0, 0), "snap_align": False,
-                                                                 "snap_normal": (0, 0, 0), "gpencil_strokes": False,
-                                                                 "cursor_transform": False, "texture_space": False,
-                                                                 "remove_on_cancel": False, "view2d_edge_pan": False,
-                                                                 "release_confirm": False, "use_accurate": False,
+                                                                 "snap_target": 'CLOSEST', "snap_point": (0, 0, 0),
+                                                                 "snap_align": False, "snap_normal": (0, 0, 0),
+                                                                 "gpencil_strokes": False, "cursor_transform": False,
+                                                                 "texture_space": False, "remove_on_cancel": False,
+                                                                 "view2d_edge_pan": False, "release_confirm": False,
+                                                                 "use_accurate": False,
                                                                  "use_automerge_and_split": False})
     bpy.ops.object.make_single_user(object=True, obdata=True, material=True, animation=False, obdata_animation=False)
 
     for obj in bpy.context.selected_objects:
-        obj.name = "Meter." + str(i + 1)
+        obj.name = meter + str(i + 1)
 
     bpy.ops.object.select_all(action='DESELECT')
-    bpy.data.objects["Meter." + str(i + 1)].select_set(True)
+    bpy.data.objects[meter + str(i + 1)].select_set(True)
 
     bpy.context.object.modifiers["Hook-Empty"].object = bpy.data.objects["Aspect"]
 
