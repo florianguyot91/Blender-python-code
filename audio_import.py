a3dac7a2d2c8b4a531206a67e5db444051de689f
Guyot Florian <florian.guyot1@epfedu.fr>
2023-04-18 19:11:38 +0200

    Solved errors

--- audio_import.py
+++ audio_import.py
@@ -2,22 +2,23 @@ import bpy
 
 # Set the filepath to the audio file
 audio_filepath = ".\Blender-python-code\Post Malone, Swae Lee - Sunflower (Spider-Man_ Into the Spider-Verse).mp3"
+audio_name = "Build_a_Castle.mp3"
 
 # Switch to the VSE workspace
 bpy.context.area.ui_type = 'SEQUENCE_EDITOR'
 
 # Add a new audio strip to the VSE
 bpy.ops.sequencer.sound_strip_add(
-    filepath="//Post Malone, Swae Lee - Sunflower (Spider-Man_ Into the Spider-Verse).mp3",
+    filepath="//" + audio_name,
     directory="Blender-python-code\\",
     files=[{
-        "name": "Post Malone, Swae Lee - Sunflower (Spider-Man_ Into the Spider-Verse).mp3",
-        "name": "Post Malone, Swae Lee - Sunflower (Spider-Man_ Into the Spider-Verse).mp3"}],
+        "name": audio_name,
+        "name": audio_name}],
     relative_path=True, frame_start=0, channel=1)
 
 # Get the audio strip from the VSE
 audio_strip = bpy.data.scenes["Scene"].sequence_editor.sequences_all[
-    "Post Malone, Swae Lee - Sunflower (Spider-Man_ Into the Spider-Verse).mp3"]
+    audio_name]
 
 # Set the end frame of the audio strip
 audio_strip.frame_final_end = int(audio_strip.frame_start + audio_strip.frame_final_duration)
