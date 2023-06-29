192d742e1fec472435eaa7f4c64c4a0521a8d81b
Guyot Florian <florian.guyot1@epfedu.fr>
2023-06-30 00:07:02 +0200

    Merged file import and audio processing Added an intensity meter

--- blender script.py
+++ blender script.py
@@ -2,8 +2,36 @@ import math
 import bpy
 import os
 
+bpy.ops.sequencer.delete()
+
+# Set the filepath to the audio file
+audio_name = "No Sleep Till Brooklyn.mp3"
+audio_filepath = ".\Blender-python-code\Musics" + "\" + " + audio_name
+
+# Switch to the VSE workspace
+bpy.context.area.ui_type = 'SEQUENCE_EDITOR'
+
+# Add a new audio strip to the VSE
+bpy.ops.sequencer.sound_strip_add(
+    filepath="//Musics\\"+audio_name,
+    directory="Blender-python-code\\Musics\\", files=[
+        {"name": audio_name,
+         "name": audio_name}], relative_path=True,
+    frame_start=1, channel=1)
+
+# Get the audio strip from the VSE
+audio_strip = bpy.data.scenes["Scene"].sequence_editor.sequences_all[
+    audio_name]
+
+# Set the end frame of the audio strip
+audio_strip.frame_final_end = int(audio_strip.frame_start + audio_strip.frame_final_duration)
+
+# Set the scene end frame to match the end of the audio strip
+bpy.context.scene.frame_end = audio_strip.frame_final_end
+bpy.context.area.ui_type = 'TEXT_EDITOR'
+
 file_path = os.path.join(os.path.dirname(bpy.data.filepath),
-                         "Musics/Rhapsody - The Magic of the Wizards Dream.mp3")
+                         "Musics/" + audio_name)
 original_type = bpy.context.area.type
 nb_barres = 20
 bar_spacing_x = 1.1
@@ -14,18 +42,37 @@ zLocation = bpy.data.objects["Aspect"].location.z
 bpy.data.objects["Aspect"].location = tempLocation
 min_frequency = 20
 max_frequency = 20000
+context = bpy.context
+
+bpy.data.objects[meter + str(1)].select_set(True)
+
+# bpy.data.objects[meter + str(1)].animation_data = None
+
+# bpy.data.materials["Meter material Full." + "001"].node_tree.nodes["Value.002"].outputs[0].keyframe_delete(
+#     data_path='default_value', frame=1)
+
+bpy.data.objects["Aspect"].select_set(True)
+bpy.data.objects["Height"].select_set(True)
+bpy.data.objects["Spacing"].select_set(True)
+bpy.data.objects["Camera"].select_set(True)
+bpy.data.objects["Intensity"].select_set(True)
+bpy.ops.object.select_all(action='INVERT')
+bpy.ops.object.delete(use_global=False, confirm=False)
+
 
 bpy.ops.outliner.orphans_purge(do_recursive=True)
 
-
-
-context = bpy.context
 
 for area in context.screen.areas:
     if area != context.area:
         break
 
 override = {'region': area.regions[0]}
+
+# bpy.ops.anim.channels_select_all(override, action='SELECT')
+# bpy.ops.anim.channels_delete(override)
+
+bpy.data.objects[meter + str(1)].select_set(True)
 
 for i in range(1, nb_barres):
 
@@ -75,6 +122,7 @@ for i in range(1, nb_barres):
 
     bpy.ops.graph.sound_bake(filepath=file_path, low=loga_frequency_min, high=loga_frequency_max, attack=0.2)
 
+bpy.ops.object.select_all(action='DESELECT')
 bpy.data.objects[meter + str(1)].select_set(True)
 
 bpy.context.area.type = "NODE_EDITOR"
