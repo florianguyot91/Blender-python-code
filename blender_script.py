1acdc1ed2c91d45c2dd663f32ed4d62d03efcecf
Guyot Florian <florian.guyot1@epfedu.fr>
2023-07-02 21:28:01 +0200

    Commit before going on vacation

--- blender_script.py
+++ blender_script.py
@@ -2,28 +2,21 @@ import math
 import bpy
 import os
 
-from bpy.props import StringProperty, BoolProperty
+from bpy.props import StringProperty
 from bpy_extras.io_utils import ImportHelper
 from bpy.types import Operator
 
+
 class OT_TestOpenFilebrowser(Operator, ImportHelper):
     bl_idname = "test.open_filebrowser"
-    bl_label = "Open the file browser (yay)"
+    bl_label = "Open the file browser"
 
     filter_glob: StringProperty(
         default='*.mp3',
         options={'HIDDEN'}
     )
 
-    # some_boolean: BoolProperty(
-    #     name='Do a thing',
-    #     description='Do a thing with the file you\'ve selected',
-    #     default=True,
-    # )
-
     def execute(self, context):
-        """Do something with the selected file(s)."""
-
         filename_path, extension = os.path.splitext(self.filepath)
         filename = os.path.basename(self.filepath)
         directory = os.path.dirname(self.filepath)
@@ -33,7 +26,6 @@ class OT_TestOpenFilebrowser(Operator, ImportHelper):
         print('File extension:', extension)
         print('File name:', filename)
         print('Directory :', directory)
-        # print('Some Boolean:', self.some_boolean)
         import_audio(self.filepath)
         audio_processing(self.filepath)
 
@@ -48,15 +40,18 @@ def unregister():
     bpy.utils.unregister_class(OT_TestOpenFilebrowser)
 
 
-def import_audio(file_path):
+def delete_f_curves():
+    ob = bpy.context.object  # active object
 
+    for fcu in ob.animation_data.action.fcurves:
+        for mod in fcu.modifiers:
+            fcu.modifiers.remove(mod)
+
+
+def import_audio(file_path):
     bpy.ops.sequencer.delete()
     # Set the filepath to the audio file
     audio_name = os.path.basename(file_path)
-    # audio_name = "A Like Supreme by SAMURAI (Refused).mp3"
-
-    audio_filepath = os.path.dirname(file_path)
-    # audio_filepath = ".\Blender-python-code\Musics" + "\" + " + audio_name
     # Switch to the VSE workspace
     bpy.context.area.ui_type = 'SEQUENCE_EDITOR'
     # Add a new audio strip to the VSE
@@ -77,13 +72,11 @@ def import_audio(file_path):
     # Set the scene end frame to match the end of the audio strip
     bpy.context.scene.frame_end = audio_strip.frame_final_end
     bpy.context.area.ui_type = 'TEXT_EDITOR'
-    file_path = os.path.join(os.path.dirname(bpy.data.filepath),
-                             "Musics/" + audio_name)
 
 
 def audio_processing(file_path):
     original_type = bpy.context.area.type
-    nb_barres = 20
+    nb_barres = 30
     bar_spacing_x = 1.1
     meter = "Meter."
     bpy.data.scenes['Scene'].frame_set(1)
@@ -94,23 +87,15 @@ def audio_processing(file_path):
     max_frequency = 20000
     context = bpy.context
     bpy.data.objects[meter + str(1)].select_set(True)
-    # bpy.data.objects[meter + str(1)].animation_data = None
-    # bpy.data.materials["Meter material Full." + "001"].node_tree.nodes["Value.002"].outputs[0].keyframe_delete(
-    #     data_path='default_value', frame=1)
-    bpy.data.objects["Aspect"].select_set(True)
-    bpy.data.objects["Height"].select_set(True)
-    bpy.data.objects["Spacing"].select_set(True)
-    bpy.data.objects["Camera"].select_set(True)
-    bpy.data.objects["Intensity"].select_set(True)
-    bpy.ops.object.select_all(action='INVERT')
-    bpy.ops.object.delete(use_global=False, confirm=False)
-    bpy.ops.outliner.orphans_purge(do_recursive=True)
+
+    # delete_f_curves()
+
+    remove_previous_meters()
+
     for area in context.screen.areas:
         if area != context.area:
             break
     override = {'region': area.regions[0]}
-    # bpy.ops.anim.channels_select_all(override, action='SELECT')
-    # bpy.ops.anim.channels_delete(override)
     bpy.data.objects[meter + str(1)].select_set(True)
     for i in range(1, nb_barres):
 
@@ -118,12 +103,14 @@ def audio_processing(file_path):
                                       (((math.log10(max_frequency - min_frequency)) / nb_barres) * i)) + min_frequency
         loga_frequency_max = math.pow(10,
                                       (((math.log10(max_frequency - min_frequency)) / nb_barres) * (
-                                                  i + 1))) + min_frequency
+                                              i + 1))) + min_frequency
 
         bpy.data.objects[meter + str(i)].select_set(True)
         context.area.type = 'VIEW_3D'
-        bpy.ops.object.duplicate_move_linked(override, OBJECT_OT_duplicate={"linked": True, "mode": 'TRANSLATION'},
-                                             TRANSFORM_OT_translate={"value": (bar_spacing_x, 0, 0), "orient_axis_ortho": 'X',
+        bpy.ops.object.duplicate_move_linked(override,
+                                             OBJECT_OT_duplicate={"linked": True, "mode": 'TRANSLATION'},
+                                             TRANSFORM_OT_translate={"value": (bar_spacing_x, 0, 0),
+                                                                     "orient_axis_ortho": 'X',
                                                                      "orient_type": 'GLOBAL',
                                                                      "orient_matrix": ((1, 0, 0), (0, 1, 0), (0, 0, 1)),
                                                                      "orient_matrix_type": 'GLOBAL',
@@ -141,8 +128,12 @@ def audio_processing(file_path):
                                                                      "view2d_edge_pan": False, "release_confirm": False,
                                                                      "use_accurate": False,
                                                                      "use_automerge_and_split": False})
-        bpy.ops.object.make_single_user(object=True, obdata=True, material=True, animation=False,
-                                        obdata_animation=False)
+        bpy.ops.object.make_single_user(object=True,
+                                        obdata=True,
+                                        material=True,
+                                        animation=False,
+                                        obdata_animation=False
+                                        )
 
         for obj in bpy.context.selected_objects:
             obj.name = meter + str(i + 1)
@@ -174,6 +165,18 @@ def audio_processing(file_path):
     bpy.ops.graph.sound_bake(filepath=file_path, low=20, high=loga_frequency_first_bar, attack=0.2)
     bpy.data.objects["Aspect"].location.z = zLocation
     bpy.context.area.type = original_type
+
+
+def remove_previous_meters():
+    bpy.data.objects["Aspect"].select_set(True)
+    bpy.data.objects["Height"].select_set(True)
+    bpy.data.objects["Spacing"].select_set(True)
+    bpy.data.objects["Camera"].select_set(True)
+    bpy.data.objects["Intensity"].select_set(True)
+    bpy.ops.object.select_all(action='INVERT')
+    bpy.ops.object.delete(use_global=False, confirm=False)
+    bpy.ops.outliner.orphans_purge(do_recursive=True)
+
 
 if __name__ == "__main__":
     register()
